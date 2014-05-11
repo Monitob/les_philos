cd178bc52c2cb33b33c259144257ea196c39e021
Jaime BERNABE <jbernabe@e3r12p21.42.fr>
2014-05-11 06:05:26 +0200

    as

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jbernabe <jbernabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/09 04:33:38 by jbernabe          #+#    #+#             */
-/*   Updated: 2014/05/10 07:50:23 by jbernabe         ###   ########.fr       */
+/*   Updated: 2014/05/11 05:44:41 by jbernabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,29 @@
 # define THINK_T		2 // nombre de segs pour reflechir
 # define THINK_OUT		5 // nombre de segs pour arreter la simulation
 # define THREADS		7
+# define EBUSY			0
+
+typedef struct		s_thread
+{
+	int				life;
+	int 			ph_id;
+	int				ph_state;
+	pthread_t		philosopher_id;
+	pthread_mutex_t	*m_left;
+	pthread_mutex_t	*m_right;	
+}					t_thread;
 
 typedef struct		s_philo
 {
 	void			*mlx;
 	void			*win;
-	pthread_t		philosopher_id[THREADS];
-	int 			ph_id;
-	int				ph_state;
-	int				point_php_life;			
+	t_thread		philo_st[THREADS];
+	int				nb;
 }					t_philo;
 
+void	init_left_right_mutex(t_thread *philosopher_st, int nb);
+void	*fct_thread_son(void *param);
+void	init_maieutic(t_philo *table);
 // void	*fct_thread_son(void	*param);
 // t_philo	*init_maieutic(t_philo *table);
 
--- src/main.c
+++ src/main.c
@@ -6,92 +6,138 @@
 /*   By: jbernabe <jbernabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/14 17:47:16 by jbernabe          #+#    #+#             */
-/*   Updated: 2014/05/10 08:09:18 by jbernabe         ###   ########.fr       */
+/*   Updated: 2014/05/11 05:57:20 by jbernabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <mlx.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "libft.h"
 #include "philo.h"
 
-pthread_mutex_t		sticks[THREADS];
+pthread_mutex_t		g_sticks[THREADS];
 
-void 	init_mutex(t_philo *table)
+void 	init_mutex(void)
 {
 	int 	i;
 
 	i = 0;
 	while (i < THREADS)
 	{
-		if (pthread_mutex_init(&(sticks[i]), NULL))
+		if (pthread_mutex_init(&(g_sticks[i]), NULL))
 			return ;
 		i++;
 	}
-	(void)table;
-}
-
-void	start_dinner(t_philo *table)
-{
-	int 	i;
-
-	i = MAX_LIFE;
-	pthread_mutex_lock(&sticks[i]);
-	while (i > 0)
-	{
-
-		i--;
-	}
-	pthread_mutex_unlock(&sticks[i]);
 }
 
 void	*fct_thread_son(void *param)
 {
-	int i;
+	t_philo	*table;
+	int		nb;
+	int		rizq;
+	int 	rrigh;
 
-	i = 0;
-	pthread_mutex_lock(&sticks[i]);
-	while (1)
-	{
-		printf("thread param %d and i-> %d\n", *((int *)param), );
-		i++;
-		sleep (1);
-	}
-	pthread_mutex_unlock(&sticks[i]);
-	pthread_exit(0);
+
+	table = (t_philo *)param;
+	nb = table->nb;
+	
+		rizq = pthread_mutex_trylock(table->philo_st[nb].m_left);
+		if (rizq == EBUSY)
+			rrigh = pthread_mutex_trylock(table->philo_st[nb].m_right);
+		if (rizq == EBUSY && rrigh == EBUSY)
+		{
+			printf("EATIN THE SUSHI!\n");
+			printf("je suis le thread numero %d\n mes points de vie sont %d\n mon mutex de gauche c'est %p\n mon mutex de droite c'est %p\n", nb,
+			table->philo_st[nb].life, table->philo_st[nb].m_left, table->philo_st[nb].m_right);
+			if ((usleep(EAT_T)) == EBUSY)
+			{
+				if (table->philo_st[nb].life != MAX_LIFE)
+					table->philo_st[nb].life = MAX_LIFE;
+				pthread_mutex_unlock(table->philo_st[nb].m_left);
+				pthread_mutex_unlock(table->philo_st[nb].m_right);
+			}		
+		}
+		if ((rrigh != EBUSY && rizq == EBUSY) || (rrigh == EBUSY && rizq != EBUSY))
+		{
+			printf("philo in THINKING\n");
+			printf("je suis le thread numero %d\n mes points de vie sont %d\n mon mutex de gauche c'est %p\n mon mutex de droite c'est %p\n", nb,
+				table->philo_st[nb].life, table->philo_st[nb].m_left, table->philo_st[nb].m_right);
+			if (usleep(THINK_T) == 0)
+		 		table->philo_st[nb].life--;
+			if (table->philo_st[nb].life == 0)
+			{
+				pthread_mutex_destroy(table->philo_st[nb].m_left);
+				pthread_mutex_destroy(table->philo_st[nb].m_right);
+			}
+
+		}
+		if (rrigh != EBUSY && rizq != EBUSY)
+		{
+			printf("philo in rest ZZZZzzZZ\nb");
+			printf("je suis le thread numero %d\n mes points de vie sont %d\n mon mutex de gauche c'est %p\n mon mutex de droite c'est %p\n", nb,
+		 	table->philo_st[nb].life, table->philo_st[nb].m_left, table->philo_st[nb].m_right);
+		 	if (usleep(REST_T) == 0)
+		 		table->philo_st[nb].life--;
+			if (table->philo_st[nb].life == 0)
+			{
+				pthread_mutex_destroy(table->philo_st[nb].m_left);
+				pthread_mutex_destroy(table->philo_st[nb].m_right);
+			}
+		}
+	
+	return (0);
+
 }
 
-// PTHREAD_CREATE_JOINABLE-> on peut attendre pour le thread(fil)
-// PTHREAD_CREATE_DETACHED -> on libere automatiquement
+void	init_left_right_mutex(t_thread *philosopher_st, int nb)
+{
+	if (nb == 0)
+	{
+		philosopher_st->life = MAX_LIFE;
+		philosopher_st->m_left = &g_sticks[6];
+		philosopher_st->m_right = &g_sticks[nb];
+		philosopher_st->ph_state = 0;
+	}
+	else
+	{
+		philosopher_st->life = MAX_LIFE;
+		philosopher_st->m_left = &g_sticks[nb - 1];
+		philosopher_st->m_right = &g_sticks[nb];
+		philosopher_st->ph_state = 0;
+	}
+}
 
 void		creat_threats(t_philo *table)
 {
-	int		i;
-	void	*ret;
+	// void	*ret;
 
-	i = 0;
-	while (i < THREADS)
+	table->nb = 0;
+	while (table->nb < THREADS)
 	{
-		if (pthread_create(&(table->philosopher_id[i]), NULL, fct_thread_son, &sticks[i]))
+		// define life to MAX_LIFE
+		// call function init_lr_mutex()
+		init_left_right_mutex(&(table->philo_st[table->nb]), table->nb);
+		if (pthread_create(&(table->philo_st[table->nb].philosopher_id), NULL, fct_thread_son, table))
 		{
 			ft_putendl("Couldn't create a philosopher");
 			return ;
 		}
-		i++;
+		usleep(500);
+		table->nb++;
 	}
-	start_dinner(table);
-	i = 0;
-	while (i < THREADS)
-    {
-        pthread_join(table->philosopher_id[i], &ret);
-        i++;
-    }
+	// i = 0;
+	// while (i < THREADS)
+ //    {
+ //        pthread_join(table->philosopher_id[i], &ret);
+ //        i++;
+ //    }
 }
 
 void	init_maieutic(t_philo *table)
 {
-	init_mutex(table);
+	init_mutex();
 	creat_threats(table);
 }
 
@@ -100,7 +146,6 @@ int		main(void)
 	t_philo		table;
 
 	init_maieutic(&table);
-
 	// init_mutex(&table);
 	// wait_threats(table);
 	//table.mlx = mlx_init();
